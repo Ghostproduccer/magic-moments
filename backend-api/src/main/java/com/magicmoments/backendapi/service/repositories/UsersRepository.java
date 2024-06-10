5bd62385d22070414fdaae9cf842819d2f550b51
BrunoRF <brunorf15@gmail.com>
2024-06-10 12:50:33 +0200

    separados repos

--- backend-api/src/main/java/com/magicmoments/backendapi/service/repositories/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.magicmoments.backendapi.service.repositories;
-
-import com.magicmoments.backendapi.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-}
